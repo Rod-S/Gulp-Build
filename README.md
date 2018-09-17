af3c49d2a6d24954a3b8d654ea67c3923913080e
Rod-S <33836692+Rod-S@users.noreply.github.com>
2018-09-17 17:17:01 -0500

    -clean functionality working correctly -server automatically refreshing on sass changes

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Using Gulp to Build a Front End Website
+
+From the command line run:
+
+$ npm install
+$ gulp
+
+console will log on successful deployment:
+//
+Server started http://localhost:3000
+LiveReload started on port '..'
+Running server
+//
--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,22 @@
 "use strict";
-
-var gulp = require('gulp'),
+//node modules/dependencies
+var fs = require('fs'),
+    gulp = require('gulp'),
     concat = require('gulp-concat'),
     uglify = require('gulp-uglify'),
+    smushit = require('gulp-smushit'),
     rename = require('gulp-rename'),
     sass = require('gulp-sass'),
     maps = require('gulp-sourcemaps'),
-    del = require('del'),
-    smushit = require('gulp-smushit'),
-    connect = require('gulp-connect'),
-    fs = require('fs'),
     rimraf = require('rimraf'),
-    deleteEmpty = require('delete-empty');
+    del = require('del'),
+    connect = require('gulp-connect'),
+    livereload = require('gulp-livereload'),
+    runSeq = require('run-sequence');
 
-gulp.task('scripts', ['clean'], function() {
+//compile, concat, minify js files with addt'l source map file
+//send to /dist/js
+gulp.task('scripts', function() {
     return gulp.src([
       'js/circle/autogrow.js',
       'js/circle/circle.js',
@@ -27,39 +30,54 @@ gulp.task('scripts', ['clean'], function() {
     .pipe(gulp.dest('dist/js'));
 });
 
+//compile/concatenate sass to css and minify with addt'l source map file
+//send to /dist/css
 gulp.task('styles', function() {
     return gulp.src("sass/global.scss")
     .pipe(maps.init())
     .pipe(sass({outputStyle: 'compressed'}))
     .pipe(rename('all.min.css'))
     .pipe(maps.write('./'))
-    .pipe(gulp.dest('dist/css'));
+    .pipe(gulp.dest('dist/css'))
+    .pipe(livereload({start: true}));
 });
 
-gulp.task('images', ['clean'], function() {
+//optimize jpg and png images inside /images with smushit()
+//send to /dist/content
+gulp.task('images', function() {
     return gulp.src("images/*.{jpg,png}")
     .pipe(smushit())
-    .pipe(gulp.dest('dist/content'))
+    .pipe(gulp.dest('dist/content'));
 });
 
+//watch for changes to scss files in sass folder
+//run styles to recompile/reminify when changes detected
+//run livereload to refresh browser
 gulp.task('watchFiles', function() {
+  livereload.reload();
   gulp.watch('sass/**/*.scss', ['styles']);
-})
+});
 
+//Remove dist folder for necessary tasks
 gulp.task('clean', function() {
-  rimraf('dist', function() {
+  rimraf('dist/*', function() {
     if (fs.existsSync('dist')) {
-      fs.unlinkSync('dist');
-      fs.rmdirSync('dist');
-      deleteEmpty('dist');
+      del('dist/*');
     }
   });
 });
 
-gulp.task("build", ['clean', 'scripts', 'styles', 'images']);
-
-gulp.task("serve",["build", 'watchFiles'], function() {
-  connect.server({port:3000});
+//run and complete clean task; followed by scripts, styles and images in parallel
+gulp.task("build", function() {
+  return runSeq('clean', ['scripts', 'styles', 'images']);
 });
 
+//serve app on localhost:3000 with livereload enabled to automatically refresh changes
+gulp.task("serve", ["images", 'watchFiles'], function() {
+  connect.server({
+    port:3000,
+    livereload: true
+  });
+});
+//default gulp task to run build and serve tasks
 gulp.task("default", ["build", "serve"]);
